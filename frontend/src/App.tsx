b0f2d7e9e0f73a89b8d44f203d7e1d76b396873f
dagak <dkrenich@student.agh.edu.pl>
2026-01-10 16:25:33 +0100

    brand slider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import Navbar from "./shared/components/Navbar/Navbar";
 import Slider from "./modules/auth/components/slider/Slider";
 import "./App.css";
+import BrandStrip from "./modules/auth/components/brandstrip/BrandStrip";
 
 function App() {
   return (
     <>
       <Navbar />
       <Slider />
-
+      <BrandStrip />
     </>
   );
 }
---
+++ frontend/src/modules/auth/components/brandstrip/BrandStrip.tsx
@@ -0,0 +1,32 @@
+import "./BrandStrip.css"
+import boj from "./boj2.png"
+import cosrx from "./cosrx.png"
+import dralthea from "./dralthea.png"
+import imfrom from "./imfrom.png"
+import round_lab from "./round_lab.png"
+
+const brands = [
+    { name: "Beauty of Joseon", logo: boj },
+    { name: "Cosrx", logo: cosrx },
+    { name: "Dr Althea", logo: dralthea },
+    { name: "I'm from", logo: imfrom },
+    { name: "Round Lab", logo: round_lab }
+];
+
+export default function BrandStrip() {
+    return (
+        <section className="brand-strip">
+            <div className="brand-strip__logos">
+                {brands.map((brand) => (
+                    <img
+                        key={brand.name}
+                        src={brand.logo}
+                        alt={brand.name}
+                        className="brand-strip__logo"
+                    />
+                ))}
+            </div>
+
+        </section>
+    )
+}
