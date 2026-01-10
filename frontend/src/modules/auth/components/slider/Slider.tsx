ee55e01f2e154de5c0e4a2fe1959d708cc13ee0d
dagak <dkrenich@student.agh.edu.pl>
2026-01-10 15:46:00 +0100

    adding slider

---
+++ frontend/src/modules/auth/components/slider/Slider.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import "./Slider.css"
+
+const images = [
+    "frontend\cosmetics1.jpg",
+    "frontend\cosmetics2.jpg",
+    "frontend\cosmetics3.jpg",
+    "frontend\cosmetics4.jpg"
+]
+export default function Slider() {
+    const [currentIndex, setCurrentIndex]
+}
