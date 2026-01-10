2223edd95fe6011ae7e79f402f2ff369198bf3ed
Natalia <147085553+nxtalia@users.noreply.github.com>
2026-01-10 21:35:21 +0100

    Auth fix

---
+++ backend/src/main/java/com/pixiehex/kshipping/services/WeeklyTaskScheduler.java
@@ -0,0 +1,22 @@
+package com.pixiehex.kshipping.services;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@Component
+public class WeeklyTaskScheduler {
+
+    private final BatchService batchService;
+
+    public WeeklyTaskScheduler(BatchService batchService) {
+        this.batchService = batchService;
+    }
+
+    // Every Monday at 00:00
+    @Scheduled(cron = "0 0 0 * * MON")
+    public void runWeeklyBatching() {
+        batchService.processBatching();
+    }
+}
