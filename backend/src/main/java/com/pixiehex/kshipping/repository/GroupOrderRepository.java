abdd9a87437d9a48d2297bd609525c475d29fa8c
Natalia Kuchta <147085553+nkucht4@users.noreply.github.com>
2026-01-10 16:46:24 +0100

    Delete backend directory

--- backend/src/main/java/com/pixiehex/kshipping/repository/GroupOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pixiehex.kshipping.repository;
-
-import com.pixiehex.kshipping.model.GroupOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupOrderRepository extends JpaRepository<GroupOrder, Long> {
-}
--- backend/src/main/java/com/pixiehex/kshipping/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pixiehex.kshipping.repository;
-
-import com.pixiehex.kshipping.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
