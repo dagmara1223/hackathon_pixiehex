cf583699fb18141dcd7edd355c5911d0029ef967
dagak <dkrenich@student.agh.edu.pl>
2026-01-10 16:43:51 +0100

    Added About section

--- frontend/src/modules/home/pages/HomePage.tsx
+++ frontend/src/modules/home/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 // modules/home/pages/HomePage.tsx
 import Slider from "../../auth/components/slider/Slider";
 import BrandStrip from "../../auth/components/brandstrip/BrandStrip";
+import AboutSection from "./sections/AboutSection";
 export default function HomePage() {
   return (
     <>
       <Slider />
       <BrandStrip></BrandStrip>
+      <AboutSection />
     </>
   );
 }
---
+++ frontend/src/modules/home/pages/sections/AboutSection.tsx
@@ -0,0 +1,22 @@
+import "./AboutSection.css";
+
+export default function AboutSection() {
+    return (
+        <section className="about">
+            <div className="about__content">
+                <h2>W paczce siła</h2>
+
+                <p>
+                    PIXIEHEX to platforma do wspólnych zamówień kosmetyków i produktów
+                    prosto z Korei Południowej. Łączymy osoby zainteresowane tymi samymi
+                    produktami, aby obniżyć koszty importu i uprościć cały proces.
+                </p>
+
+                <p>
+                    Zamiast kilku osobnych paczek — jedna zbiorcza wysyłka, mniej kosztów i
+                    zero stresu z odprawą.
+                </p>
+            </div>
+        </section>
+    );
+}
