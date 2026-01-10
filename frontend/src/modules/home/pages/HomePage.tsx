6aa3d6d5903f60e5395f227da4338b65a8ad3400
DELL <kalinaraczka@gmail.com>
2026-01-10 18:10:38 +0100

    main form update

--- frontend/src/modules/home/pages/HomePage.tsx
+++ frontend/src/modules/home/pages/HomePage.tsx
@@ -1,13 +1,14 @@
-// modules/home/pages/HomePage.tsx
 import Slider from "../../auth/components/slider/Slider";
 import BrandStrip from "../../auth/components/brandstrip/BrandStrip";
 import AboutSection from "./sections/AboutSection";
+import MainForm from "./MainForm";
 export default function HomePage() {
   return (
     <>
       <Slider />
       <BrandStrip></BrandStrip>
       <AboutSection />
+      <MainForm/>
     </>
   );
 }
