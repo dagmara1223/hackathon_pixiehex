fb64f9be46ba56a790dc8300e5c00a4c99779bbe
dagak <dkrenich@student.agh.edu.pl>
2026-01-10 17:35:00 +0100

    Added about me and finished

--- frontend/src/modules/home/pages/sections/AboutSection.tsx
+++ frontend/src/modules/home/pages/sections/AboutSection.tsx
@@ -1,4 +1,6 @@
 import "./AboutSection.css";
+import boxImg from "./box.png";
+import peopleImg from "./people.png"
 
 export default function AboutSection() {
     return (
@@ -16,6 +18,33 @@ export default function AboutSection() {
                     Zamiast kilku osobnych paczek — jedna zbiorcza wysyłka, mniej kosztów i
                     zero stresu z odprawą.
                 </p>
+
+                {/*co nas wyroznia section */}
+                <div className="about__features">
+                    <h2>Co nas wyróżnia?</h2>
+
+                    <div className="about__cards">
+                        <div className="about__card">
+                            <img src={peopleImg} alt="Pojedyncze zamówienie" />
+                            <h3>Przyjazna społeczność, szybko i drama - free! </h3>
+                            <p>
+                                Tworzymy miejsca, w których ludzie szybko się dogadują, wspólnie
+                                wybierają produkty i dzielą doświadczenia. Bez chaosu, bez
+                                przypadkowych ofert – tylko sprawdzone zamówienia.
+                            </p>
+                        </div>
+
+                        <div className="about__card about__card--highlight">
+                            <img src={boxImg} alt="Wspólne zamówienie PIXIEHEX" />
+                            <h3>Wspólne zamówienia PIXIEHEX</h3>
+                            <p>
+                                Zamiast wielu osobnych paczek – jedna zbiorcza przesyłka.
+                                Mniej kosztów, mniej formalności i pełna kontrola nad procesem
+                                importu z Korei Południowej.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
