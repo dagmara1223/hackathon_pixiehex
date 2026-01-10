0681689e50330d121ac4bb147fcf810384350e8b
Natalia <147085553+nxtalia@users.noreply.github.com>
2026-01-11 00:12:52 +0100

    AA

--- backend/src/main/java/com/pixiehex/kshipping/services/WeeklyTaskScheduler.java
+++ backend/src/main/java/com/pixiehex/kshipping/services/WeeklyTaskScheduler.java
@@ -9,14 +9,17 @@ import org.springframework.stereotype.Component;
 public class WeeklyTaskScheduler {
 
     private final BatchService batchService;
+    private SingleOrderService singleOrderService;
 
-    public WeeklyTaskScheduler(BatchService batchService) {
+    public WeeklyTaskScheduler(BatchService batchService, SingleOrderService singleOrderService) {
         this.batchService = batchService;
+        this.singleOrderService = singleOrderService;
     }
 
     // Every Monday at 00:00
     @Scheduled(cron = "0 0 0 * * MON")
     public void runWeeklyBatching() {
         batchService.processBatching();
+        singleOrderService.changeUnpaidToCancelled();
     }
 }
