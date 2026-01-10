0a01db531876f8eb73ea6f7e2898d7d1df773881
DELL <kalinaraczka@gmail.com>
2026-01-10 22:56:22 +0100

    errors in main form

--- frontend/src/modules/home/pages/MainForm.tsx
+++ frontend/src/modules/home/pages/MainForm.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./mainform.css";
 import { ProductGrid } from "./ProductGrid";
-
+import * as z from 'zod';
 type Product = {
     id: number;
     name: string;
@@ -9,6 +9,29 @@ type Product = {
     region: string;
     weight: number;
 };
+
+export const contactSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Imię i nazwisko są za krótkie")
+    .regex(/^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$/, "Użyj tylko liter"),
+    
+  mail: z
+    .string()
+    .email("To nie jest poprawny adres e-mail"),
+    
+  address: z
+    .string()
+    .min(5, "Adres musi mieć co najmniej 5 znaków"),
+    
+  postal_code: z
+    .string()
+    .regex(/^\d{2}-\d{3}$/, "Kod pocztowy musi mieć format 00-000"),
+    
+  city: z
+    .string()
+    .min(2, "Nazwa miasta jest za krótka")
+});
 
 function extractBrand(productName: string): string {
     const knownBrands = [
@@ -32,7 +55,7 @@ export default function MainForm() {
     const [chosenProducts, setChosenProducts] = useState<number[]>([]);
     const [showConfirmModal, setShowConfirmModal] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
-
+    const [errors, setErrors] = useState("");
     const [userData, setUserData] = useState({
         name: "",
         mail: "",
@@ -75,11 +98,26 @@ export default function MainForm() {
 
     // produkty danej marki
     const filteredProducts = products.filter(
-        p => extractBrand(p.name) === chosenBrand
-    );
+            p => extractBrand(p.name) === chosenBrand
+        );
 
+    const handleCheck = () =>{
+            const validation = contactSchema.safeParse(userData);
+    
+            if (!validation.success) {
+                // Mapujemy błędy Zod na stan Twoich komunikatów w UI
+                //const errorMessages = validation.error.map(err => err.message);
+                const pretty = z.prettifyError(validation.error);
+                setErrors(pretty);
+                return false;
+    }
+    
+        setErrors(""); // Czyścimy błędy, jeśli wszystko OK
+        return true;
+    }
     // submit
     const handleSubmit = async (e: React.FormEvent) => {
+        
         e.preventDefault()
         try {
             if (chosenProducts.length === 0) {
@@ -126,7 +164,7 @@ export default function MainForm() {
         <div className="mainform-container">
             <div className="order-card">
                 <h2>Zbierz grupę i zamów już teraz!</h2>
-
+                
                 {/* MARKA */}
                 <div className="selection-section">
                     <label>Marka:</label>
@@ -163,7 +201,7 @@ export default function MainForm() {
                         />
                     </div>
                 )}
-
+                {errors && <p style={{color: 'red'}}>{errors}</p>}
                 {/* FORMULARZ */}
                 {chosenProducts.length > 0 && (
                     <div className="user-details-form">
@@ -171,6 +209,9 @@ export default function MainForm() {
                         <form
                             onSubmit={(e) => {
                                 e.preventDefault();
+                                if(!handleCheck()){
+                                    return;
+                                }
                                 setShowConfirmModal(true);
                             }}
                         >
